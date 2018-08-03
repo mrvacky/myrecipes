fe221a06ec51e9ee568ba6e29fe1f2398e74a2c8
vacky <varunchowthai@gmail.com>
2018-08-03 16:23:55 +0530

    add one to many association between recipe and chef

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -4,5 +4,6 @@ validates :chefname, presence: true, length: { maximum: 30}
 VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 validates :email, presence: true, length: { maximum: 255 },
 				  format: { with: VALID_EMAIL_REGEX },
-				  uniqueness: {case_sensitive: false}	
+				  uniqueness: { case_sensitive: false }	
+has_many :recipes				  
 end
